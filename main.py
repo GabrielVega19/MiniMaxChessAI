41077697ffd27da896bdda04d97ba5a7a83ed637
gabriel <vegabito1112@gmail.com>
2022-11-14 16:27:15 -0500

    changes

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from gym_chess import ChessEnvV1, ChessEnvV2
 
 env = ChessEnvV2()
 env = gym.make('ChessVsSelf-v2')
+env.reset()
 
 currentState = env.state
 moves = env.possible_moves
 action = env.move_to_action(moves[0])
 
+currentState, reward, done, info = env.step(action)
 
 
-print(action)
+env.render()
 
