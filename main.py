748baa940c8c83eb44864fcf574cb71b62e16a44
gabriel <vegabito1112@gmail.com>
2022-11-14 16:56:59 -0500

    Project layout

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import gym
 from gym_chess import ChessEnvV1, ChessEnvV2
 
+
+# Shows state of board
+# Loop
+    # Enter prompt for piece and move (requires correct move)
+        #turn input into touple of touples((6,0), (5,0))
+        #turn into action
+    # Apply move and show board
+        #apply move to the board from the action
+    # Then display ai is moving and generate ai’s move
+        #print out something to show ai is moving
+        #use minimax to generate move
+            #need to make a utility function
+            #Make the tree
+                #Nodes of board state(for utility function), move that was used to get there, and the minimax value
+            #start from bottom of the tree and then go upwards
+            #you would look at children chose the one with the chosen minimax value then chose the move at that child node
+        #turn move into action 
+    # Show board after ai’s move
+        #apply move to the board from the action
+
+
+
 env = ChessEnvV2()
 env = gym.make('ChessVsSelf-v2')
 env.reset()
@@ -9,8 +31,9 @@ currentState = env.state
 moves = env.possible_moves
 action = env.move_to_action(moves[0])
 
-currentState, reward, done, info = env.step(action)
+newState, reward, done, info = env.step(action)
 
+print(currentState)
 
 env.render()
 
